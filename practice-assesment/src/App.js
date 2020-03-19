a7bf97d1eb8290291b3c336933850c40d4947b06
dannyl <danilovlaurent@pursuit.org>
2020-03-19 12:53:12 -0400

    organize files

--- practice-assesment/src/App.js
+++ practice-assesment/src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
+import "./reset.css";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <>
+      <div className="nav">NAV</div>
+      <div className="content">CONTENT</div>
+    </>
+  );
 }
 
 export default App;
